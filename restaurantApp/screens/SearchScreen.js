240a016bac130a953dcdb556329c2c559542efb3
lprtan <lprntan@gmail.com>
2024-10-30 17:25:47 +0300

    filter restaurant by price for back-end

--- restaurantApp/screens/SearchScreen.js
+++ restaurantApp/screens/SearchScreen.js
@@ -2,13 +2,22 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import SearchBar from '../components/SearchBar'
 import useResult from '../hooks/useResult'
+import ResultList from '../components/ResultList'
 
 export default function SearchScreen() {
     const [searchApi, results] = useResult();
-    //console.log(results);
+    
+    const filterResultByPrice = (price) => {
+      return results.filter((results)=> {
+        return results.price === price;
+      })
+    }
   return (
     <View>
       <SearchBar/>
+      <ResultList title = {'Ucuz Restoranlar'} results = {filterResultByPrice('₺')}/>
+      <ResultList title = {'Uygun Restoranlar'} results = {filterResultByPrice('₺₺')}/>
+      <ResultList title = {'Pahalı Restoranlar'} results = {filterResultByPrice('₺₺₺')}/>
     </View>
   )
 }
