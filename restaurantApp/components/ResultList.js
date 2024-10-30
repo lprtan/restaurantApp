47add75b0d76185a7de7c25eb90822b9f60aee8c
lprtan <lprntan@gmail.com>
2024-10-30 17:46:32 +0300

    Restaurants Shown on Screen

--- restaurantApp/components/ResultList.js
+++ restaurantApp/components/ResultList.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import ResultDetail from './ResultDetail'
 
 export default function ResultList( {title, results }) {
   return (
     <View>
-      <Text>ResultList</Text>
+      <Text>{title}</Text>
+      <FlatList
+      data={results}
+      renderItem={({item})=>{
+        return(
+            <TouchableOpacity>
+                <ResultDetail result = {item}/>
+            </TouchableOpacity>
+        )
+      }}
+      />
     </View>
   )
 }
