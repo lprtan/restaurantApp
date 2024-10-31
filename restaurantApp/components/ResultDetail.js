b980aa29c99f46ef8dbb5829ecd70d8e020d6e98
lprtan <lprntan@gmail.com>
2024-10-31 17:20:52 +0300

    added css in restaurant list

--- restaurantApp/components/ResultDetail.js
+++ restaurantApp/components/ResultDetail.js
@@ -3,12 +3,25 @@ import React from 'react'
 
 export default function ResultDetail({ result }) {
   return (
-    <View>
-      <Image style= {{width: 150, height: 120 }} source={result.image_url ? {uri: result.image_url} : null}/> 
-      <Text>{result.name}</Text> 
+    <View style = {styles.container}>
+      <Image style= {styles.image} source={result.image_url ? {uri: result.image_url} : null}/> 
+      <Text style = {styles.resturantName}>{result.name}</Text> 
       <Text>{result.rating} Yıldızlı Restoran, {result.review_count} Değerlendirme</Text>    
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15
+  },
+  image: {
+    width: 250, 
+    height: 120,
+    borderRadius: 8,
+    marginBottom: 5
+  },
+  resturantName: {
+    fontWeight: 'bold'
+  }
+})
