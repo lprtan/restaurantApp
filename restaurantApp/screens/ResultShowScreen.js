a310d83c1929c630290028faad69c14c991df3d8
lprtan <lprntan@gmail.com>
2024-11-02 13:15:37 +0300

    added restuarant detail data show

--- restaurantApp/screens/ResultShowScreen.js
+++ restaurantApp/screens/ResultShowScreen.js
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
 
-export default function ResultShowScreen() {
+export default function ResultShowScreen({route}) {
+    const [result, setResult] = useState(null);
+    const id = route.params.id;
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+      };
+
+    useEffect(()=>{
+        getResult(id);
+    },[])
+
+    if(!result){
+        return null
+    }
   return (
     <View>
-      <Text>ResultShowScreen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.phone}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return (
+            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
+          );
+        }}
+      />
     </View>
   )
 }
